60fbc7f13a45a2d27b4ed8676a63562853648430
Luke Imhoff <luke_imhoff@rapid7.com>
2014-08-29 08:34:34 -0500

    Update version for branch
    MSP-11068

--- lib/metasploit/yard/version.rb
+++ lib/metasploit/yard/version.rb
@@ -9,7 +9,7 @@ module Metasploit
       # The patch number, scoped to the {MINOR} version number.
       PATCH = 2
       # The pre-release version, scoped to the {PATCH} version number.
-      PRERELEASE = 'semantic-versioning'
+      PRERELEASE = 'port'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
