b3426d170affdf5dbf782d9892ff26f797e93ac1
Lance Sanchez <lance.sanchez@gmail.com>
2016-02-25 11:22:25 -0600

    bumping the version to 2.0.0
    removing the bundler lock

--- lib/metasploit/yard/version.rb
+++ lib/metasploit/yard/version.rb
@@ -3,12 +3,13 @@ module Metasploit
     # Holds components of {VERSION} as defined by {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0}.
     module Version
       # The major version number.
-      MAJOR = 1
+      MAJOR = 2
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 0
       # The patch number, scoped to the {MINOR} version number.
-      PATCH = 2
-
+      PATCH = 0
+      
+      PRERELEASE = 'rails-upgrade'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the PRERELEASE in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
